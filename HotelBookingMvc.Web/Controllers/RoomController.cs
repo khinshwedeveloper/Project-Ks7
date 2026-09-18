d41a67d1f7ff46d13e55fc340ee6a9255c53e8fd
Khin shwe <khinnshwe.developer@gmail.com>
2026-09-18 15:59:40 +0630

    Feat:Room Update

--- HotelBookingMvc.Web/Controllers/RoomController.cs
+++ HotelBookingMvc.Web/Controllers/RoomController.cs
@@ -1,37 +1,133 @@
-﻿using HotelBookingMvc.Web.Services;
+﻿using Hotelbooking.Database.Models;
+using HotelBookingMvc.Web.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hotelbooking.Database.Context;
 
-namespace HotelBookingMvc.Web.Controllers
+namespace HotelBookingMvc.Web.Controllers;
+
+public class RoomController : Controller
 {
-    public class RoomController : Controller
-    {
-        private readonly Room_Service _roomService;
-        private readonly AppDbContext _db;
+    private readonly Room_Service _roomService;
+    private readonly AppDbContext _db;
 
-        public RoomController(Room_Service roomService)
-        {
-            _roomService = roomService;
-        }
+    public RoomController(
+        Room_Service roomService,
+        AppDbContext db)
+    {
+        _roomService = roomService;
+        _db = db;
     }
+
+
     public async Task<IActionResult> Index()
+    {
+        var rooms = await _roomService.GetRoomsAsync();
+
+        ViewBag.RoomTypes = await _db.TblRoomTypes
+            .OrderBy(x => x.RoomTypeName)
+            .ToListAsync();
+
+        return View(rooms);
+    }
+
+    
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.RoomTypes = await _db.TblRoomTypes
+            .OrderBy(x => x.RoomTypeName)
+            .ToListAsync();
+
+        return View();
+    }
+
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(TblRoom room)
+    {
+        if (!ModelState.IsValid)
         {
-            var roooms = await _roomService.GetRoomsAsync();
             ViewBag.RoomTypes = await _db.TblRoomTypes
                 .OrderBy(x => x.RoomTypeName)
                 .ToListAsync();
-            return View(rooms);
+
+            return View(room);
         }
-        [HttpGet]
-        public async Task<IActionResult> Create()
+
+        bool result = await _roomService.CreateRoomAsync(room);
+        if (result)
+        {
+            TempData["Success"] = "Room created successfully.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        TempData["Error"] = "Unable to create room.";
+
+        ViewBag.RoomTypes = await  _db.TblRoomTypes
+            .OrderBy(x => x.RoomTypeName)
+            .ToListAsync();
+
+        return View(room);
+    }
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var room = await _roomService.GetRoomByIdAsync(id);
+        if (room == null)
+        {
+            return NotFound();
+        }
+        ViewBag.RoomTypes = await _db.TblRoomTypes
+            .OrderBy(x => x.RoomTypeName)
+            .ToListAsync();
+        return View(room);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult>Edit(TblRoom room)
+    {
+        if (!ModelState.IsValid)
         {
             ViewBag.RoomTypes = await _db.TblRoomTypes
-                .(x=> x.RoomTypeName)
-                .ToListAsync()
-
-
-
-                return View();
+                .OrderBy(x => x.RoomTypeName)
+                .ToListAsync();
+            return View(room);
         }
 
+        bool result = await _roomService.UpdateRoomAsync(room);
+        if(result)
+        {
+            TempData["Success"] = "Room Update Succesfully.";
+            return RedirectToAction(nameof(Index));
+        }
+        TempData["Error"] = "Unable to update room.";
 
+        ViewBag.RoomTypes = await _db.TblRoomTypes
+            .OrderBy(x => x.RoomTypeName)
+            .ToListAsync();
+        return View(room);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult>Delete(int id)
+    {
+        bool result = await _roomService.DeleteRoomAsync(id);
+
+        if(result)
+        {
+            TempData["Success"] = "Room delete succesfully.";
+        }
+        else
+        {
+            TempData["Error"] = "Unable to delete";
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
 }
--- HotelBookingMvc.Web/obj/Debug/net8.0/HotelBookingMvc.Web.AssemblyInfo.cs
+++ HotelBookingMvc.Web/obj/Debug/net8.0/HotelBookingMvc.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingMvc.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3c0be03df3c5290a734a5b0777753d12bb7031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94dec70148243456c142aadd4a30e3e0dbdf1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingMvc.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingMvc.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
